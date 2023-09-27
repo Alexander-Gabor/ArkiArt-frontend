863b8e35f51c6937602e067ece23f9575429978c
Alexander-Gabor <gaboralexanderalexgmail.com>
2023-09-27 15:02:47 +0200

    Backend API link updated

--- src/utils/urls.jsx
+++ src/utils/urls.jsx
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://arkiart-backend-sgcqkoiltq-no.a.run.app'
+// const BASE_URL = 'https://arkiart-backend-sgcqkoiltq-no.a.run.app'
+const BASE_URL = 'https://arkiart-auth.onrender.com/'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
